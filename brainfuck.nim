78a378333f57d026b1e959b1ce58bb43b41bcaef
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2021-12-15 15:00:15 +0100

    some minor update

--- brainfuck.nim
+++ brainfuck.nim
@@ -1,5 +1,6 @@
 import os
 
+const stackLimit: int = 30000
 let paramsList: seq[string] = commandLineParams()
 
 if len(paramsList) == 0: echo "Brainfuck interpreter v0.1\n\nusage: brainfuck [<file>]"
@@ -8,10 +9,12 @@ elif len(paramsList) == 1:
 
   var
     source: seq[char]
-    stack: array[30000, int]
+    stack: array[stackLimit, int]
     stackIndex: int = 0
     sourceIndex: int = 0
-    loopIndex: seq[int]
+    loopStack: seq[int]
+    loopStart: int = 0
+    loopEnd: int = 0
 
   for character in input:
     source.add(character)
@@ -32,10 +35,8 @@ elif len(paramsList) == 1:
       else: dec stack[stackIndex]
     of '.': stdout.write stack[stackIndex].char()
     of ',': stack[stackIndex] = readChar(stdin).int()
-    of '[': loopIndex.add(sourceIndex)
-    of ']':
-      if stack[stackIndex] == 0: loopIndex.delete(loopIndex[^1])
-      else: sourceIndex = loopIndex[^1]
+    of '[': discard
+    of ']': discard
     else: discard
 
     inc sourceIndex
