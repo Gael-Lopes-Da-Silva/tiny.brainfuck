655dfaa658f04d7fa50210a90c4dcff835173d04
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2022-01-04 16:30:16 +0100

    ⚙️add some doc

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 set -xe
 
-dotnet publish -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true
+# you can replace win-x64 by linux-x64 or osx.10.11-x64
+# you can also replace x64 by x86 for x86 computers
+
+dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true
