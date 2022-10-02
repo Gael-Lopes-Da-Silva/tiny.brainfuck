a7e2ceefa65f236e82a2ebfd0eee7706154dbd78
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2022-10-02 12:04:13 +0200

    CHANGED: removed releases and added a build folder with builds

--- Source/brainfuck.cs
+++ Source/brainfuck.cs
@@ -2,6 +2,7 @@
 @author: Gael Lopes Da Silva
 @project: Brainfuck Interpreter
 @github: https://github.com/Gael-Lopes-Da-Silva/Brainfuck
+@gitlab: https://gitlab.com/Gael-Lopes-Da-Silva/Brainfuck
 */
 
 class Brainfuck
